450d88c1aca41c8a4d83165e779b09ec730601b8
Alexandre Gréau <alexgreau@live.fr>
2020-07-23 18:40:35 +0200

    Routing

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import HOME from './constants/Routes';
 
-import Layout from './hoc/Layout/Layout'
+import Layout from './hoc/Layout/Layout';
 
 function App() {
   return (
+
     <div className="App light-theme">
-      <Layout>
-        
-      </Layout>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Redirect to={HOME}></Redirect>
+          </Route>
+          <Route path={HOME} component={Layout}></Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ website/src/constants/Routes.js
@@ -0,0 +1,4 @@
+export const HOME = '/home';
+export const PROJECTS = '/projects';
+
+export default HOME;
