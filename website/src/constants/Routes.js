c29dc35f0ae69b1f2e23c445cbf57cdbdeb16654
Alexandre Gréau <alexgreau@live.fr>
2020-07-25 16:37:09 +0200

    Changed profile pic

--- website/src/constants/Routes.js
+++ website/src/constants/Routes.js
@@ -1,6 +1,10 @@
+// Routes
 export const HOME = '/home';
 export const PROJECTS = '/projects';
+
+// To files
+export const PROFILE_PIC = "Me/pic_of_me.jpg";
 export const RESUME = 'Me/Resume_2020.pdf';
-export const PROFILE_PIC = "Me/photo_me.jpeg"
+
 
 export default HOME;
