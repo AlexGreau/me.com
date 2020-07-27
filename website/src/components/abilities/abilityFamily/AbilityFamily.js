c8838d34fc8ceec1808c6b731605a01af945236f
Alexandre Gréau <alexgreau@live.fr>
2020-07-27 22:37:24 +0200

    Distinct columns and animation on text too

--- website/src/components/abilities/abilityFamily/AbilityFamily.js
+++ website/src/components/abilities/abilityFamily/AbilityFamily.js
@@ -4,24 +4,30 @@ import Ability from '../ability/Ability';
 
 const AbilityFamily = (props) => {
 
-    const skillsList = (
+    const skillsList = () =>{
+        const half_length = Math.ceil(props.list.length / 2);
+        const firstHalf = [...props.list].splice(0,half_length);
+        const secondHalf = [...props.list].splice(half_length, props.list.length)
+        
+        return (
         <div className={classes.skillsList}>
             <div className={classes.col}>
-                {props.list.map(skill => {
+                {firstHalf.map(skill => {
                     return <Ability key={skill[0]} name={skill[0]} value={skill[1]} />
                 })}
             </div>
             <div className={classes.col}>
-                {props.list.map(skill => {
+                {secondHalf.map(skill => {
                     return <Ability key={skill[0]} name={skill[0]} value={skill[1]} />
                 })}
             </div>
         </div>
-    )
+    )}
+
     return (
         <div className={classes.family}>
             <h3 className={classes.title}>{props.name} :</h3>
-            {skillsList}
+            {skillsList()}
         </div>
     )
 }
