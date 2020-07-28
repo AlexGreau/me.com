80ef0501d42ece0f72a34441a92f8095069a1c9f
Alexandre Gréau <alexgreau@live.fr>
2020-07-29 00:05:13 +0200

    Start of contactMe

--- website/src/components/contactMe/ContactMe.js
+++ website/src/components/contactMe/ContactMe.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import classes from './ContactMe.module.css';
+import Map from '../ui/map/Map';
+import ContactBar from '../ui/contactBar/ContactBar';
 
 const ContactMe = (props) => {
     return (
-        <div ref={props.ref}>
+        <div ref={props.ref} className={classes.contactMe}>
             <h2 className={props.familyTitleStyle}>Say hello !</h2>
-            <p>map of where i am</p>
-            <p>List of contact points OR Form</p>
+            <div className={classes.container}>
+                <Map />
+                <div className={classes.contactOptions}>
+                    <p>List of contact points OR Form</p>
+                    <ContactBar/>
+
+                </div>
+            </div>
         </div>
 
     )
---
+++ website/src/components/ui/map/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Map.module.css';
+
+const Map = (props) => { 
+    return (
+        <div className={classes.map}>
+            Map of where I am 
+        </div>
+    )
+}
+export default Map;
