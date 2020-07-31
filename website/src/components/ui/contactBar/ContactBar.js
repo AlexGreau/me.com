b20196a6bd57258d083979c0ac691c33db5b922b
Alexandre Gréau <alexgreau@live.fr>
2020-07-31 17:29:59 +0200

    Resume Component

--- website/src/components/ui/contactBar/ContactBar.js
+++ website/src/components/ui/contactBar/ContactBar.js
@@ -6,31 +6,15 @@ import { FirebaseContext } from '../../Firebase/server';
 import CV_icon from '../../../assets/contactLogos/CV_icon.png'
 
 import ContactItem from './contactItem/ContactItem';
+import Resume from '../resume/Resume';
 
 
 const ContactBar = (props) => {
-
-    const buildCV = (firebase) => {
-        /* <button className={classes.resume} onClick={firebase.getCV}>Resume</button> */
-        const CV = {
-            id: 0,
-            name: "Resume",
-            img: CV_icon
-        }
-
-        return (
-            <ContactItem key={CV.id} social={CV} clicked={firebase.getCV} />
-        )
-    }
-
     return (
         <div className={classes.Container}>
             <div className={classes.ContactBar}>
-                <FirebaseContext.Consumer >
-                    {firebase => {
-                        return buildCV(firebase);
-                    }}
-                </FirebaseContext.Consumer>
+                {/* Adding resume icon to the bar */}
+                <Resume/>
                 {
                     CONTACTS.map(SOCIAL => {
                         return (
