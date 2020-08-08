c1e22ba2387a776e9ed1005145e7b29843df1b8e
Alexandre Gréau <alexgreau@live.fr>
2020-08-08 16:58:21 +0200

    Card margins changed according to material guidelines

--- website/src/components/ui/card/smallCard/SmallCard.js
+++ website/src/components/ui/card/smallCard/SmallCard.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import classes from './SmallCard.module.css';
+import Button from '../../button/Button';
 
 const SmallCard = (props) => {
     return (
         <div className={classes.container}>
             <div className={classes.header}>
                 <div className={classes.title}>Title</div>
-                <div className={classes.img}>img</div>
+                <div className={classes.company}>Company</div>
             </div>
             <div className={classes.body}>
                 Phasellus finibus, libero et rhoncus cursus, purus mauris rutrum ipsum, sit amet faucibus massa urna at felis.
             </div>
-            <div className={classes.actionSection}>
-                Actions
-            </div>
-
+            {/* <div className={classes.actionSection}>
+                <Button>See more</Button>
+            </div> */}
         </div>
     )
 
