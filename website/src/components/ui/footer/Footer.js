fd36e2dbb33725fa16cb880a9aa0ad045f89ce0b
Alexandre Gréau <alexgreau@live.fr>
2020-08-18 22:26:31 +0200

    Added footer to skeleton

---
+++ website/src/components/ui/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+const Footer = (props) => {
+ return (
+<div className={classes.container}>
+ <p>This is a Footer</p> 
+</div>)
+}
+export default Footer;
--- website/src/containers/Home/Home.js
+++ website/src/containers/Home/Home.js
@@ -5,6 +5,7 @@ import pres from '../../components/description/self/SelfPres'
 import SelfPres from '../../components/description/self/SelfPres';
 import Skillset from '../../components/skillset/Skillset';
 import ContactMe from '../../components/contactMe/ContactMe';
+import Footer from '../../components/ui/footer/Footer';
 
 const Home = (props) => { 
     return (
@@ -19,13 +20,14 @@ const Home = (props) => {
             <div>
                 <ContactMe/>
             </div>
+            <Footer/>
 
-            <div>The goal of this screen is to have a very short intro to the website :</div>
+            {/* <div>The goal of this screen is to have a very short intro to the website :</div>
             <ul>
                 <li>A navbar to be able to  shortcut to any pages</li>
                 <li>small version of every sections, to intrigue the visitor and start building a relation ship</li>
                 <li>A hire me button, in case the user is convinced</li>
-            </ul>
+            </ul> */}
 
         </div>
     )
